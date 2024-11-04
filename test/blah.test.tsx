1cd4ecae290134adc7d7ad06898609d26910be99
Indranil Halder <indranilhalder.dev@gmail.com>
2024-11-05 00:21:19 +0530

    Test updates

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { MaterialButton } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const button = document.createElement('button');
+    ReactDOM.render(<MaterialButton />, button);
+    ReactDOM.unmountComponentAtNode(button);
   });
 });
